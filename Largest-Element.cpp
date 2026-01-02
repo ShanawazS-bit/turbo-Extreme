b195e647c411e8e25fc9a1a3216d7d86c87cb87a
Shanawaz Sharif <shanawaz@shanawazs-MacBook-Air.local>
2026-01-02 23:30:30 +0300

    Your commit message

---
+++ Largest-Element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int max = nums[0];
+        for (int i = 0; i < sizeof(nums); i++)
+        {   
+            if (nums[i] > max)
+            {
+                max = nums[i];
+
+
+                
+            }
+            
+        }
+        return max;
+
+        
+    }
+};
